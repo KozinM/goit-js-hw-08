eab72e30b7b5fa1fa64ff2390e757fff002e73aa
Maxim <31155487+KozinM@users.noreply.github.com>
2022-08-29 22:49:19 +0300

    another commit
    another commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+
+/* finding gallery element*/
+const galleryElement = document.querySelector(".gallery");
+
+/* creating array of strings of future gallery elements*/
+const galleryContentStringsArray = galleryItems.map(element=>{ return `<a class="gallery__item" href="${element.original}">
+<img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+</a>`});
+
+/* creating gallery items*/
+galleryElement.insertAdjacentHTML("beforeend", galleryContentStringsArray.join(" "));
+
+/* adding event listiner for handling clicks on gallery elements*/
+galleryElement.addEventListener('click', clickOnGalleryElementHandler);
+
+/* defining clickOnGalleryElementHandler*/
+function clickOnGalleryElementHandler(event) {
+    if (event.target.nodeName!=="IMG"){
+    return;
+    }
+    event.preventDefault();
+    new SimpleLightbox ('.gallery a', {
+        nav: true,
+        close: true,
+        caption: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+}
 console.log(galleryItems);
