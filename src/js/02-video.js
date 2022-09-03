836f2b7fdd4dfd425835a54b8590a767d548b79b
Maxim <31155487+KozinM@users.noreply.github.com>
2022-09-03 10:04:19 +0300

    partially done 2 task
    from the 8-th homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
 
+    import Player from '@vimeo/player';
+
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    setPlayerTime();
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    player.on('timeupdate', onTimeUpdateHandler);
+
+    function onTimeUpdateHandler (data) {
+       //console.log(data.seconds);
+       localStorage.setItem("videoplayer-current-time", `${data.seconds}`);
+       //console.log(localStorage.getItem("videoplayer-current-time"));
+    }
+
+    function setPlayerTime()
+    {
+        if (!localStorage.getItem("videoplayer-current-time"))
+        return;
+        player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+    }
