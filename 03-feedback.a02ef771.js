b110693f5d85bfec8cef17703f783a218e57c20f
KozinM <31155487+KozinM@users.noreply.github.com>
2022-09-04 17:30:52 +0000

    Deploying to gh-pages from @ KozinM/goit-js-hw-08@23034a465c39edadbf82803cd0bdf2ab27de2a8e 🚀

--- 03-feedback.a02ef771.js
+++
@@ -1,2 +0,0 @@
-!function(){const e={form:document.querySelector(".feedback-form"),mail:document.querySelector("[type=email]"),message:document.querySelector("[name=message]"),localStorageData:JSON.parse(localStorage.getItem("feedback-form-state"))};e.form.addEventListener("input",(function(e){}))}();
-//# sourceMappingURL=03-feedback.a02ef771.js.map
