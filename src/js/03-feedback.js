b12af380db4d0009b95f9192ea9fbfab502b61e4
Maxim <31155487+KozinM@users.noreply.github.com>
2022-09-04 19:28:38 +0300

    Update 03-feedback.js
    first steps

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    mail: document.querySelector("[type=email]"),
+    message: document.querySelector("[name=message]"),
+    localStorageData: JSON.parse(localStorage.getItem("feedback-form-state")),
+}
 
+refs.form.addEventListener('input', formInputHandler);
+/* onStartLoad(); */
+/* console.log(refs.localStorageData); */
+
+
+function onStartLoad()
+{
+    if(!localStorage.getItem("feedback-form-state")) {
+        return;
+    }
+    refs.mail.value = localStorageData.mail;
+    refs.message.value = localStorageData.message;
+    /* refs.mail.value = */
+};
+
+function formInputHandler (event) {
+    
+}
