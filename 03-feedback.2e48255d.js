e4fbb029f119e6a25939781e083952f6584645ea
KozinM <31155487+KozinM@users.noreply.github.com>
2022-09-05 04:34:16 +0000

    Deploying to gh-pages from @ KozinM/goit-js-hw-08@020581a11df1472fd3085732d27e004af9aa9901 🚀

--- 03-feedback.2e48255d.js
+++
@@ -1,2 +0,0 @@
-!function(){const e={form:document.querySelector(".feedback-form"),mail:document.querySelector("[type=email]"),message:document.querySelector("[name=message]"),localStorageData:JSON.parse(localStorage.getItem("feedback-form-state"))};e.form.addEventListener("input",(function(e){console.log(e.currentTarget)}))}();
-//# sourceMappingURL=03-feedback.2e48255d.js.map
--- 03-feedback.621d05c4.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),mail:document.querySelector("[type=email]"),message:document.querySelector("[name=message]"),localStorageData:JSON.parse(localStorage.getItem("feedback-form-state"))};e.form.addEventListener("input",(function(e){console.log(e.currentTarget)}));
-//# sourceMappingURL=03-feedback.621d05c4.js.map
